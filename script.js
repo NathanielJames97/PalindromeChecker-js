1143f4a9e22b58e12ce60884f9f0aa603496fc3a
NathanielJames <65167045+NathanielJames97@users.noreply.github.com>
2024-06-03 10:54:07 +0100

    add first event listener.

---
+++ script.js
@@ -0,0 +1,14 @@
+const inputWord = document.getElementById("text-input");
+
+const checkButton = document.getElementById("check-btn");
+
+// Listener for the button, saving the value for use.
+
+checkButton.addEventListener("click", function() {
+const inputValue = inputWord.value;
+
+let savedValue = inputValue;
+
+// test it's working
+
+});
