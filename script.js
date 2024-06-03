a340a89eec820dd121c58ee41401cc7b34175f24
NathanielJames <65167045+NathanielJames97@users.noreply.github.com>
2024-06-03 12:40:38 +0100

    Removed alphanumeric charecters and test.

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ const checkButton = document.getElementById("check-btn");
 // Listener for the button, saving the value for use.
 
 checkButton.addEventListener("click", function() {
-const inputValue = inputWord.value;
+    // Use the correct variable name that references the input field
+    const inputValue = inputWord.value;
+  
+    let savedValue = inputValue;
 
-let savedValue = inputValue;
+    savedValue = savedValue.replace(/[^a-z0-9]/gi, '');
 
-// test it's working
+    alert("The cleaner value is" + savedValue);
 
-});
+    if (inputValue == "") {
+        alert("Please input a value");
+    }
+  
+    //alert("Entered value is:" + savedValue);  // Add colon after "is"
+  
+    // Close the anonymous function parenthesis
+  });
+  
